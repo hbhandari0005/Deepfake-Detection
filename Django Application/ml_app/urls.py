21f9c4bccd0959cc723f8e2a97a1dd64179af6f2
Harshit bhandari <hbhandari0005@gmail.com>
2026-06-11 01:47:10 +0530

    Remove unused files and clean up templates by deleting admin, models, and test files; update URLs and footer navigation.

--- Django Application/ml_app/urls.py
+++ Django Application/ml_app/urls.py
@@ -10,7 +10,5 @@ handler404 = views.handler404
 
 urlpatterns = [
     path('', index, name='home'),
-    path('about/', about, name='about'),
     path('predict/', predict_page, name='predict'),
-    path('cuda_full/',cuda_full,name='cuda_full'),
 ]
